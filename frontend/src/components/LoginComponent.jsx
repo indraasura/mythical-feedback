743e1698f372461f0ae10dd5b11ca1b8db6d4aff
Shashank Sharma <shashank.sharma98@gmail.com>
2019-05-16 22:17:27 +0530

    Login components edit

--- frontend/src/components/LoginComponent.jsx
+++ frontend/src/components/LoginComponent.jsx
@@ -12,22 +12,26 @@ class LoginComponent extends Component {
         return (
             <div>
                 <div className={"row"} style={{marginBottom: 0}}>
-                    <div className={"col s4 center"} style={{backgroundColor: "grey", minHeight: "100vh"}}>
-                        <div className={"input-field col s10 center white-text"}>
-                            <input placeholder="Phone Number" id={"login-phone_number"} type="text" className={"validate"} />
-                            <input placeholder="Password" id={"login-password"} type="password" className={"validate"}
-                                   onFocus={() => {
-                                this.setState({
-                                    backgroundColor: "#212121",
-                                    mythicalLogo: mythicalNegative
-                                })
-                            }}
-                            onBlur={() => {
-                                this.setState({
-                                    backgroundColor: "white",
-                                    mythicalLogo: mythical
-                                })
-                            }}/>
+                    <div className={"col s4 center"} style={{backgroundColor: "#e3f2fd", minHeight: "100vh"}}>
+                        <div className="card" style={{margin: "20px", display: "block", marginTop: "40%"}}>
+                            <div className="card-content black-text">
+                                <span className="card-title">Login</span>
+                                <input placeholder="Phone Number" id={"login-phone_number"} type="text" className={"validate"} />
+                                <input placeholder="Password" id={"login-password"} type="password" className={"validate"}
+                                       onFocus={() => {
+                                           this.setState({
+                                               backgroundColor: "#212121",
+                                               mythicalLogo: mythicalNegative
+                                           })
+                                       }}
+                                       onBlur={() => {
+                                           this.setState({
+                                               backgroundColor: "white",
+                                               mythicalLogo: mythical
+                                           })
+                                       }}/>
+                            </div>
+                            <button className={'btn btn-large'} style={{float: 'left'}}>Submit</button>
                         </div>
                     </div>
                     <div className={"col s8 logo-background"} style={{backgroundColor: this.state.backgroundColor, minHeight: "100vh"}}>
