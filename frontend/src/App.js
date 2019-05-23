da0bfc8aeb55d1875627ec0f3aec10fc2f78f15c
Shashank Sharma <shashank.sharma98@gmail.com>
2019-05-23 21:19:45 +0530

    Fix Heroku Bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Main from './components/main';
 import Footer from './components/footer';
 import ExcelData from './components/exceldata';
 import ScriptBuilder from './components/ScriptBuilder';
 import LoginComponent from './components/LoginComponent';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import M from 'materialize-css'
 import './App.css'
 import SpinnerComponent from "./components/SpinnerComponent";
@@ -13,7 +13,8 @@ class App extends Component {
     state = {
         loading: true,
     };
-    componentDidMount(){
+
+    componentDidMount() {
         M.AutoInit();
         setTimeout(() => {
             this.setState({
@@ -21,22 +22,25 @@ class App extends Component {
             })
         }, 1000)
     }
+
     render() {
         return (
-            <Router>
+            <div>
                 <SpinnerComponent isLoading={this.state.loading}/>
-                <div>
-                    <Switch>
-                        <Route exact path='/' component={Main} />
-                        <Route path='/about' component={Main} />
-                        <Route path='/excel' component={ExcelData} />
-                        <Route path='/script-builder' component={ScriptBuilder} />
-                        <Route path='/login' component={LoginComponent} />
-                    </Switch>
-                    {/*<Footer />*/}
-                </div>
-            </Router>
-         );
+                <Router>
+                    <div>
+                        <Switch>
+                            <Route exact path='/' component={Main}/>
+                            <Route path='/about' component={Main}/>
+                            <Route path='/excel' component={ExcelData}/>
+                            <Route path='/script-builder' component={ScriptBuilder}/>
+                            <Route path='/login' component={LoginComponent}/>
+                        </Switch>
+                        {/*<Footer />*/}
+                    </div>
+                </Router>
+            </div>
+        );
     }
 }
 
