5196fa8cc9534588b1e8b0e6991abf7bdf1e028d
Shashank Sharma <shashank.sharma98@gmail.com>
2019-05-23 22:13:02 +0530

    HomePage Started, Gradient Background

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import FileUpload from './fileupload'
 import M from 'materialize-css'
-
+import './main.css';
 
 class Main extends Component {
     state = {  }
@@ -13,16 +13,20 @@ class Main extends Component {
     render() { 
         return ( 
             <div>
-                <nav className = "nav-bg">
-                    <div className="nav-wrapper">
-                        <ul id="nav-mobile" className="right hide-on-med-and-down">
-                            <li><Link to={'/'}>Home</Link></li>
-                            <li><Link to={'/excel'}>View Sheet</Link></li>
-                            <li><Link to={'/script-builder'}>Script Builder</Link></li>
-                            <li><Link to={'/contact'}>Contact Us</Link></li>
-                        </ul>
-                    </div>
-                </nav>
+                {/*<nav className = "nav-bg">*/}
+                    {/*<div className="nav-wrapper">*/}
+                        {/*<ul id="nav-mobile" className="right hide-on-med-and-down">*/}
+                            {/*<li><Link to={'/'}>Home</Link></li>*/}
+                            {/*<li><Link to={'/excel'}>View Sheet</Link></li>*/}
+                            {/*<li><Link to={'/script-builder'}>Script Builder</Link></li>*/}
+                            {/*<li><Link to={'/contact'}>Contact Us</Link></li>*/}
+                        {/*</ul>*/}
+                    {/*</div>*/}
+                {/*</nav>*/}
+                <div className={"main-background-grad-1"}>
+                </div>
+                <div className={"main-background-grad-2"}></div>
+                <div className={"main-background-grad-3"}></div>
                 <FileUpload />
                 
             </div>
