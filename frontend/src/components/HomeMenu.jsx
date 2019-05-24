12d40468e183cb2a73b23353898d68e4a341ad29
Shashank Sharma <shashank.sharma98@gmail.com>
2019-05-24 21:51:02 +0530

    Gradient Transition, Images

--- frontend/src/components/HomeMenu.jsx
+++ frontend/src/components/HomeMenu.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import './HomeMenu.css';
+import M from "materialize-css/dist/js/materialize.min";
 
 class HomeMenu extends Component {
     state = {};
 
     componentDidMount() {
-
+        document.addEventListener('DOMContentLoaded', function () {
+            const elems = document.querySelectorAll('.tooltipped');
+            M.Tooltip.init(elems, {enterDelay: 300});
+        });
     }
 
     render() {
@@ -13,9 +17,9 @@ class HomeMenu extends Component {
             <div>
                 {(this.props.active) ?
                     <div className="wrap" style={{position: "fixed", left: this.props.positionX-240,
-                        top: this.props.positionY-240}}>
-                        <a href="#">
-                            <div></div>
+                        top: this.props.positionY-240, zIndex: 2}} onMouseLeave={this.props.resetMenuHandler}>
+                        <a>
+                            <div onMouseOver={this.props.firstMenuHandler.bind(this, 'hel')}></div>
                         </a>
                         <a href="#">
                             <div></div>
@@ -27,7 +31,8 @@ class HomeMenu extends Component {
                             <div></div>
                         </a>
                         <a href="#">
-                            <div></div>
+                            <div>
+                            </div>
                         </a>
                     </div> : <div></div>}
             </div>
--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -4,6 +4,7 @@ import FileUpload from './fileupload'
 import M from 'materialize-css'
 import './main.css';
 import HomeMenu from "./HomeMenu";
+import mouse_left from '../resources/mouse_left.png';
 
 class Main extends Component {
     state = {
@@ -28,11 +29,35 @@ class Main extends Component {
         })
     }
 
+    changeColors() {
+        console.log('Changing');
+        document.getElementsByClassName('main-background-grad-1')[0].classList.add('main-background-grad-1-hover');
+    }
+
+    resetMenuHandler = () => {
+        setTimeout(() => {
+            this.setState({
+                activeMenu: false,
+            })
+        }, 500)
+    };
+
+    firstMenuHandler = (e) => {
+        console.log('HELLO', e);
+        this.changeColors();
+    };
+
     render() {
         return (
             <div>
-                <HomeMenu positionX={this.state.coordX} positionY={this.state.coordY} active={this.state.activeMenu}/>
+                <HomeMenu positionX={this.state.coordX} positionY={this.state.coordY} active={this.state.activeMenu}
+                          resetMenuHandler={this.resetMenuHandler}
+                          firstMenuHandler={this.firstMenuHandler}/>
                 <div onClick={this.showCoords.bind(this)}>
+                    <div className={"center"} style={{position: "absolute", bottom: "10px", left: "90vh", float: "left", zIndex: 2}}>
+                    <img src={mouse_left} style={{width: "50px"}} />
+                        <div className={"white-text"} style={{fontSize: "12px"}}><i>Click Anywhere to Navigate</i></div>
+                    </div>
                     {/*<nav className = "nav-bg">*/}
                     {/*<div className="nav-wrapper">*/}
                     {/*<ul id="nav-mobile" className="right hide-on-med-and-down">*/}
@@ -45,9 +70,9 @@ class Main extends Component {
                     {/*</nav>*/}
                     <div className={"main-background-grad-1"}>
                     </div>
-                    <div className={"main-background-grad-2"}></div>
-                    <div className={"main-background-grad-3"}></div>
-                    <FileUpload/>
+                    {/*<div className={"main-background-grad-2"}></div>*/}
+                    {/*<div className={"main-background-grad-3"}></div>*/}
+                    {/*<FileUpload/>*/}
 
                 </div>
             </div>
